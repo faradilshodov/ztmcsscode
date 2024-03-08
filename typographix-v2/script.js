4f2eead4dce1ef58ed9fd8389e66eb005eb3f255
faradilshodov <farrukh.dilshodov@gmail.com>
2024-03-08 18:00:34 -0500

    Add custom scrollbar and navbar

--- typographix-v2/script.js
+++ typographix-v2/script.js
@@ -39,3 +39,17 @@ if (currentThemeFromLocalStorage) {
         lightMode();
     }
 }
+
+// Navigation
+const nav = document.getElementById('nav')
+const menuIcon = document.querySelector('.menu-icon')
+
+function toggleMenu() {
+    nav.classList.toggle('active')
+    menuIcon.classList.toggle('active')
+}
+
+function hideMenu() {
+    nav.classList.remove('active')
+    menuIcon.classList.remove('active')
+}
