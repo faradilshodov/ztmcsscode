2682e1d543a9a9868e9f22f54809d1180fbbecbc
faradilshodov <farrukh.dilshodov@gmail.com>
2024-03-26 15:30:42 -0400

    Add navigation bar

--- developer-portfolio/script.js
+++ developer-portfolio/script.js
@@ -1,16 +1,16 @@
 // Navigation --------------------------------
-// const nav = document.getElementById('nav')
-// const menuIcon = document.querySelector('.menu-icon')
+const nav = document.getElementById('nav')
+const menuIcon = document.querySelector('.menu-icon')
 
-// function toggleMenu() {
-//     nav.classList.toggle('active')
-//     menuIcon.classList.toggle('active')
-// }
+function toggleMenu() {
+    nav.classList.toggle('active')
+    menuIcon.classList.toggle('active')
+}
 
-// function hideMenu() {
-//     nav.classList.remove('active')
-//     menuIcon.classList.remove('active')
-// }
+function hideMenu() {
+    nav.classList.remove('active')
+    menuIcon.classList.remove('active')
+}
 
 // Slideshow ----------------------------------
 let currentImageIndex = 0;
